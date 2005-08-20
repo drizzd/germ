2466f994f89eb80b6dda8fb26a523e40e7375407
Clemens Buchacher <drizzd@aon.at>
2005-08-20 21:50:58 +0000

    update repo o authentication

--- src/cf.py
+++ src/cf.py
@@ -13,13 +13,18 @@ db_password = 'gfx17'
 buflen_max = 1024
 log_file_path = '/var/log/germ.log'
 # de, en
-lang = 'en'
+lang = 'de'
 admin_name = 'Clemens Buchacher'
 admin_email = 'drizzd@aon.at'
+
+# some more or less random string to identify password parameters
+pwd_str = 'p294e5204'
 
 ht_root = '/var/www'
 ht_path = 'lpms'
 ht_index = 'ui_ht/handler.py'
 ht_docpath = 'htdocs'
 ht_default_entity = 'users'
-ht_default_action = 'submit'
+ht_default_action = 'view'
+# change this regularly to ensure security
+ht_secret = 'asdf1234'
